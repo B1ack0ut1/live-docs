6713dc2eef03845f7a2754c9df42b2db50e7ba78
B1ack0ut1 <42845068+B1ack0ut1@users.noreply.github.com>
2024-08-01 23:22:52 +0800

    Created types folder and index.d.ts file, added in snippet of type definitions, worked on Header component

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,7 +1,18 @@
+import { Editor } from "@/components/editor/Editor";
+import Header from "@/components/Header";
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+const Document = () => {
+  return (
+    <div>
+      <Header>
+        <div className="flex w-fit items-center justify-center gap-2">
+          <p className="document-title">This is a fake document title</p>
+        </div>
+      </Header>
+      <Editor />
+    </div>
+  );
 };
 
-export default page;
+export default Document;
